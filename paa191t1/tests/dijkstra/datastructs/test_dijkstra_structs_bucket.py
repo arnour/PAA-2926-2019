74760af7c7e66b4e21ae684fd1e1168a1e3ed9a7
Diogo Munaro <diogo.mvieira@gmail.com>
2019-05-05 17:19:42 -0300

    Generalize tests for dijkstra

--- paa191t1/tests/dijkstra/datastructs/test_dijkstra_structs_bucket.py
+++ paa191t1/tests/dijkstra/datastructs/test_dijkstra_structs_bucket.py
@@ -1,5 +1,5 @@
 from paa191t1.dijkstra.datastructs.buckets import Buckets
-from paa191t1.tests.dijkstra.test_dijkstra_structs import TestStructsBase
+from paa191t1.tests.dijkstra.datastructs.test_dijkstra_structs import TestStructsBase
 
 
 class TestStructsBuckets(TestStructsBase):
---
+++ paa191t1/tests/dijkstra/test_dijkstra_bucket.py
@@ -0,0 +1,7 @@
+from paa191t1.dijkstra.datastructs.buckets import Buckets
+from paa191t1.tests.dijkstra.test_dijkstra import TestDijkstraBase
+
+
+class TestDijkstraBuckets(TestDijkstraBase):
+
+    struct = Buckets()
