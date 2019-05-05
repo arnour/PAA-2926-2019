ab4e151277bb29b723d8560ac9f4244d9bf0d0dd
Arnour Sabino <arnour.sabino@gmail.com>
2019-05-04 23:59:03 -0300

    Update doc

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # PAA-2926-2019
 Projeto e Análise de Algoritmos - PUC RIO 2019
+
+# Como desenvolver neste projeto?
+
+
+- Utilize padrão google de docstring
+- Utilize flake8 e autopep8 para lint
+- Siga o padrão de nomenclatura dos testes
+
+
+Crie um virtualenv para python 3.7 e execute:
+
+
+- Instalar as dependências do projeto
+
+```
+make setup
+```
+
+- Executar testes e lint
+
+```
+make tests
+```
+
+---
+
+
+## Diretórios e soluções
+
+- paa191t1
+    - dijkstra
+        - structs
+            - vector            
+            - heap
+            - fibonacci heap
+            - avl
+            - alfa
+            - buckets
+    - pph
+        - complexities
+            - O(n^2)
+            - O(n lg n)
+            - O(n)
+            - O(n) com pivot calculado com expressão dada
+    - frascos
+        - k_2 O($\sqrt{n}$)
+        - k_n
+        - O(lg n)
+
+
+---
--- paa191t1/dijkstra/structs/graph.py
+++ paa191t1/dijkstra/structs/graph.py
@@ -1,13 +1,50 @@
 class Graph(object):
+    """Graph é um wrapper para simplificar e abstrair detalhes de implementação de qualquer biblioteca
+     de grafos que seja utilizada neste projeto.
+
+     Examples:
+        >>> import networkx as nx
+
+        >>> dg = nx.DiGraph()
+        >>> dg.add_weighted_edges_from([(0, 1, 1), (1, 2, 1), (2, 3, 1)])
+
+        >>> graph = graph.Graph(dg)
+        
+        >>> graph.all_nodes()
+        [0, 1, 2, 3]
+        
+        >>> graph.successors(0)
+        [1]
+
+        >>> graph.edge_weight(0, 1)
+        1
+    """
 
     def __init__(self, graphx):
         self.__graphx = graphx
 
     def all_nodes(self):
+        """list(int): Retorna uma lista com todos os nós do grafo"""
         return self.__graphx.nodes()
 
     def successors(self, origin_node):
+        """Retorna uma lista com todos os nós adjascentes ao nó origem informado.
+        Args:
+            origin_node (int): O nó origem da aresta
+
+        Returns:
+            list(int): a lista de nós adjascentes
+        """
         return self.__graphx.successors(origin_node)
 
     def edge_weight(self, origin_node, target_node):
+        """Retorna o peso da aresta (origin_node, target_node)
+      
+        Args:
+            origin_node (int): O nó origem da aresta
+            target_node (int): O nó destino da aresta
+
+        Returns:
+            float: o peso da aresta
+        """
         return self.__graphx.edges[origin_node, target_node]['weight']
