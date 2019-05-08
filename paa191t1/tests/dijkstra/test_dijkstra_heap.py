a45f764b26b986e65999f47e6bba317890c33dca
Reinaldo Mozart da Gama e Silva <cmmozart@gmail.com>
2019-05-08 03:06:55 +0000

    Add Heap file

---
+++ paa191t1/tests/dijkstra/test_dijkstra_heap.py
@@ -0,0 +1,7 @@
+from paa191t1.dijkstra.datastructs.heap import Heap
+from paa191t1.tests.dijkstra.test_dijkstra import TestDijkstraBase
+
+
+class TestDijkstraVector(TestDijkstraBase):
+
+    struct = Heap()
