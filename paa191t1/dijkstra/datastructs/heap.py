217afafe9ceade7d515b832ec85bed95badf2b3d
Reinaldo Mozart da Gama e Silva <cmmozart@gmail.com>
2019-05-08 04:49:35 +0000

    Fibonacci Heap + Heap bugfix

--- paa191t1/dijkstra/datastructs/heap.py
+++ paa191t1/dijkstra/datastructs/heap.py
@@ -5,45 +5,38 @@ import heapq
 class Heap(datastructs.DijkstraDistance):
 
     def __call__(self, nodes):
-	
+
         self.__nodes = []
-		
-		self.__distances = [None] * len(nodes)
-		
-		self.__heap = []
-		
+
+        self.__distances = [None] * len(nodes)
+
+        self.__heap = []
+
         for node in nodes:
-			self.__nodes.append(node)
-			
+            self.__nodes.append(node)
+
             self.__distances[node] = math.inf
-			
-	return self
-	
-	
-	def pop(self):
-		
-		node_pair = heapq.heappop(self.__heap)
-		try:
-            while node_pair[1] not in self.__nodes:
-                node_pair = heapq.heappop(self.__heap)
-        except IndexError:
-			break
-		
-		return node_pair[1], node_pair[0]
-					
-					
-	def update(self, node, distance):
-		
-		if distance == 0:
-			self.__distances[node] = 0
-			
-			heapq.heappush(self.__heap, [self.__distances[node], node])
-		
-		else:
-			self.__distances[node] = distance
-			
-			heapq.heappush(self.__heap, [self.__distances[node], node])
-			
+
+        return self
+
+
+    def pop(self):
+        node_pair = heapq.heappop(self.__heap)        
+
+        return node_pair[1], node_pair[0]
+
+
+    def update(self, node, distance):
+        if distance == 0:
+            self.__distances[node] = 0
+
+            heapq.heappush(self.__heap, [self.__distances[node], node])
+
+        else:
+            self.__distances[node] = distance
+            
+            heapq.heappush(self.__heap, [self.__distances[node], node])
+
 
     def has_nodes_to_visit(self):
         """bool: Retorna verdadeiro se existe algum nó que ainda não foi visitado. Do contrário, falso."""
@@ -58,4 +51,4 @@ class Heap(datastructs.DijkstraDistance):
 
     @property
     def values(self):
-		return dict([(k, v) for k, v in enumerate(self.__distances) if v is not None])
+        return dict([(k, v) for k, v in enumerate(self.__distances) if v is not None])
---
+++ paa191t1/tests/dijkstra/test_dijkstra_fibonacci_heap.py
@@ -0,0 +1,7 @@
+from paa191t1.dijkstra.datastructs.fibonacci_heap import FibHeap
+from paa191t1.tests.dijkstra.test_dijkstra import TestDijkstraBase
+
+
+class TestDijkstraVector(TestDijkstraBase):
+
+    struct = FibHeap()
