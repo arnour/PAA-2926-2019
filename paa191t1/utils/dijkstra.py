435005f6494ece76f03807fb524e0d4a3e1d7222
Arnour Sabino <arnour.sabino@gmail.com>
2019-05-28 01:03:27 -0300

    Runner for buckets

--- paa191t1/utils/dijkstra.py
+++ paa191t1/utils/dijkstra.py
@@ -9,13 +9,68 @@ from almetro.instance import generator
 from almetro.complexity import Complexity
 
 
+def max_lengths():
+    return {
+        '440': 6,
+        '818': 6,
+        '1195': 6,
+        '1582': 6,
+        '1955': 6,
+        '2338': 6,
+        '2705': 6,
+        '3066': 6,
+        '3428': 6,
+        '3796': 6,
+        '4157': 6,
+        '4516': 1253,
+        '4872': 1362,
+        '5246': 1485,
+        '5606': 1598,
+        '5957': 1700,
+        '6318': 1821,
+        '6693': 1907,
+        '7046': 2040,
+        '7415': 2134,
+        '7775': 2237,
+        '8135': 2323,
+        '8497': 2451,
+        '8846': 2550,
+        '9214': 2663,
+        '9580': 2782,
+        '9940': 2876,
+        '10290': 2996,
+        '10645': 3092,
+        '10994': 3191,
+        '11349': 3296,
+        '11723': 3400,
+        '12081': 3493,
+        '12438': 3614,
+        '12798': 3730,
+        '13154': 3814,
+        '13511': 3924,
+        '13869': 4054,
+        '14240': 4094,
+        '14616': 4094,
+        '14990': 4094,
+        '15378': 4119,
+        '15750': 4121,
+        '16117': 4471,
+        '16493': 4554,
+        '16864': 4695,
+        '17238': 4780,
+        '17596': 4888,
+        '17959': 5004,
+        '18309': 5113
+    }
+
+
 def load_graph(_file, distance_struct):
     G = read_edgelist(_file, nodetype=int, data=(('weight', float),), create_using=nx.DiGraph())
     instance_name = _file.split('/')[-1].replace('instance_', '').replace('.edges', '')
     instance_size = G.number_of_nodes() + G.number_of_edges()
     return {
         'name': f'{instance_name} ({instance_size})',
-        'size': {'v': G.number_of_nodes(), 'e': G.number_of_edges()},
+        'size': {'v': G.number_of_nodes(), 'e': G.number_of_edges(), 'c': max_lengths()[str(instance_size)]},
         'value': {
             'graph': Graph(G),
             'source_node': 1,
